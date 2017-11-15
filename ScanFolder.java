35b06a008abd41ea959ec62acfafcc0082037cf7
Jatower <32618547+Jatower@users.noreply.github.com>
2017-11-14 22:41:50 -0500

    Update ScanFolder.java
    Changed return type for listFilesForFolder method, fixed bug in adding files from internal folders

--- ScanFolder.java
+++ ScanFolder.java
@@ -2,7 +2,6 @@ import java.io.File;
 
 
 public class ScanFolder {
-    public static File folder = new File("/Users/paigehanssen/Desktop/PicSorterTest");  //pathname is hardcoded rn, won't be later.
     static String temp = "";
 
     public static void main(String[] args) {
@@ -11,21 +10,25 @@ public class ScanFolder {
         listFilesForFolder(folder);
     }
 
-    public static void listFilesForFolder(final File folder) {
+    public static ArrayList<String> listFilesForFolder(final File folder) {
+        ArrayList<String> paths = new ArrayList<String>();
         for (final File fileEntry : folder.listFiles()) {
             //System.out.println(fileEntry);
             if (fileEntry.isDirectory()) {
                 System.out.println("Reading files under the folder "+folder.getAbsolutePath());
-                listFilesForFolder(fileEntry);
+                for(String path : listFilesForFolder(fileEntry)){
+                    paths.add(path);   
+                }
             } else {
                 if (fileEntry.isFile()) {
                     temp = fileEntry.getName();
                     //filter files by type
                     //if ((temp.substring(temp.lastIndexOf('.') + 1, temp.length()).toLowerCase()).equals("mov"))
-                        System.out.println("File: " + fileEntry.getName());
+                        //System.out.println("File: " + fileEntry.getName());
+                    paths.add(fileEntry);
                 }
-
             }
         }
+        return paths;
     }
 }
