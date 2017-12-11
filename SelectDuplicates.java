ee979b7f1be5bfc608863bf0d5589a517223ed64
Jatower <32618547+Jatower@users.noreply.github.com>
2017-12-11 18:03:10 -0500

    Add files via upload
    Adds functionality of selecting individual duplicates

---
+++ SelectDuplicates.java
@@ -0,0 +1,35 @@
+/*
+ * SelectDuplicates class: used to select duplicate images from GUI to be moved or deleted
+ * Author: Jacob Tower
+ */
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+public class SelectDuplicates {
+	private static SelectionArray selections = new SelectionArray();
+	private static ArrayList<JLabel> selectedImages = new ArrayList<JLabel>();
+	
+	public static void Select(Entry selection, JLabel image) {
+		if(selections.contains(selection)) {
+			image.setBorder(null);
+			selections.remove(selection);
+			selectedImages.remove(image);
+		}
+		else {
+		image.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+		selections.add(selection);
+		selectedImages.add(image);
+		}
+	}
+	
+	public static SelectionArray getSelections() {
+		return selections;
+	}
+	
+	public static ArrayList<JLabel> getImages(){
+		return selectedImages;
+	}
+	
+}
