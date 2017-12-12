4bc33334fc0a55bf7bdd03daab094a612e21e5ed
Jatower <32618547+Jatower@users.noreply.github.com>
2017-12-12 17:25:39 -0500

    Update and rename SelectDuplicates.java to Source Code/SelectDuplicates.java

--- Source Code/SelectDuplicates.java
+++ Source Code/SelectDuplicates.java
@@ -5,18 +5,22 @@
 
 import java.awt.*;
 import java.util.ArrayList;
-
 import javax.swing.*;
+
 public class SelectDuplicates {
+	//These track selected images and their JLabels
 	private static SelectionArray selections = new SelectionArray();
 	private static ArrayList<JLabel> selectedImages = new ArrayList<JLabel>();
 	
+	//selects or deselects a duplicate when it is clicked on
 	public static void Select(Entry selection, JLabel image) {
+		//deselect case
 		if(selections.contains(selection)) {
 			image.setBorder(null);
 			selections.remove(selection);
 			selectedImages.remove(image);
 		}
+		//select case
 		else {
 		image.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		selections.add(selection);
@@ -24,14 +28,17 @@ public class SelectDuplicates {
 		}
 	}
 	
+	//return selected files
 	public static SelectionArray getSelections() {
 		return selections;
 	}
 	
+	//return selected images
 	public static ArrayList<JLabel> getImages(){
 		return selectedImages;
 	}
 	
+	//return whether anything is currently selected
 	public static boolean hasSelections() {
 		if (selections.size() > 0){
 			return true;
@@ -39,9 +46,9 @@ public class SelectDuplicates {
 		else return false;
 	}
 	
+	//clear all selections
 	public static void clearSelections() {
 		selections.clear();
 		selectedImages.clear();
 	}
-	
 }
