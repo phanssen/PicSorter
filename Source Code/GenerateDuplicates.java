5a986126e552525e73c8fbebb818af222744717b
Jatower <32618547+Jatower@users.noreply.github.com>
2017-12-12 23:10:23 -0500

    Update GenerateDuplicates.java
    Added imports to support functionality

--- Source Code/GenerateDuplicates.java
+++ Source Code/GenerateDuplicates.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 
 import javax.swing.*;
